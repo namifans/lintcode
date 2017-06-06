91c502ac7082e659bfffedcd175a3aa9b8ab1af9
namifans <yanglin0617@gmail.com>
2017-06-06 00:19:18 -0700

    Longest substring without repeating characters

---
+++ LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    /**
+     * @param s: a string
+     * @return: an integer 
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int[] hash = new int[256];
+        int len = s.length(), result = 0, j = 0;
+        for (int i = 0; i < len; i ++) {
+        	while (j < len && hash[(int) s.charAt(j)] == 0) {
+    			result = Math.max(j - i + 1, result);
+    			hash[(int) s.charAt(j++)] = 1;
+        	}
+        	hash[(int) s.charAt(i)] = 0;
+        }
+        return result;
+    }
+}
