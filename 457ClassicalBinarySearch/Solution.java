1fc57517063014c8c646a20f2a589e0b6193172e
namifans <yanglin0617@gmail.com>
2017-07-03 18:43:55 -0700

    Minor fix

--- 457ClassicalBinarySearch/Solution.java
+++ 457ClassicalBinarySearch/Solution.java
@@ -5,23 +5,24 @@ public class Solution {
      * @return an integer
      */
     public int findPosition(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
     	int left = 0, right = nums.length - 1;
-    	int mid = (left + right) / 2;
     	while (left + 1 < right) {
+        	int mid = left + (right - left) / 2; // Avoid overflow
     		if (nums[mid] == target) {
     			return mid;
     		}
     		else if (nums[mid] < target) {
-    			right = mid;
-    		} else {
     			left = mid;
+    		} else {
+    			right = mid;
     		}
-    		mid = (left + right) / 2;
     	}
     	if (nums[left] == target)
     	    return left;
-    	if (nums[right] == target)
-    		return right;
+    	if (nums[right] == target) 
+    	    return right;
     	return -1;
     }
 }
