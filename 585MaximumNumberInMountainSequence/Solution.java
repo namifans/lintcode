dc970d2346d600127d1fb31f8f1fef229e8052a3
namifans <yanglin0617@gmail.com>
2017-07-04 17:36:10 -0700

    585 Maximum number in mountain sequence

---
+++ 585MaximumNumberInMountainSequence/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    /**
+     * @param nums a mountain sequence which increase firstly and then decrease
+     * @return then mountain top
+     */
+    public int mountainSequence(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return -1;
+        int left = 0, right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1])
+                left = mid;
+            else
+                right = mid;
+        }
+        return Math.max(nums[left], nums[right]);
+    }
+}
