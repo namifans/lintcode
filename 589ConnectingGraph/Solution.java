13d8e8dc897589d7b5f8ab288bf6ac0774c826a3
namifans <yanglin0617@gmail.com>
2017-08-05 23:20:19 -0700

    Remove inner class

--- 589ConnectingGraph/Solution.java
+++ 589ConnectingGraph/Solution.java
@@ -1,16 +1,13 @@
-class UnionFind {
+public class ConnectingGraph { 
+
     int[] father;
-    public UnionFind(int n) {
-        father = new int[n];
-        for (int i = 0; i < n; i++) {
-            father[i] = i;
-        }
-    }
+
     public int find(int x) {
         if (father[x] == x)
             return x;
         return father[x] = find(father[x]);
     }
+
     public void union(int x, int y) {
         int rx = find(x);
         int ry = find(y);
@@ -18,20 +15,19 @@ class UnionFind {
             father[rx] = ry;
         }
     }
-}
-public class ConnectingGraph { 
-
-    UnionFind connectGraph;
 
     public ConnectingGraph(int n) {
-        connectGraph = new UnionFind(n);
+        father = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            father[i] = i;
+        }
     }
 
     public void connect(int a, int b) {
-        connectGraph.union(a - 1, b - 1);
+        union(a, b);
     }
         
     public boolean query(int a, int b) {
-        return (connectGraph.find(a - 1) == connectGraph.find(b - 1));
+        return find(a) == find(b);
     }
 }
