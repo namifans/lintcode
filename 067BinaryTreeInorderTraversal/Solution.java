dac89531583da363eecd33a679e9c28fa4ea2c42
namifans <yanglin0617@gmail.com>
2017-08-08 23:37:02 -0700

    067 Binary tree inorder traversal

---
+++ 067BinaryTreeInorderTraversal/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (root == null)
+            return list;
+        traversal(root, list);
+        return list;
+    }
+
+    private void traversal(TreeNode root, ArrayList<Integer> list) {
+        if (root == null)
+            return;
+        traversal(root.left, list);
+        traversal(root.right, list);
+        list.add(root.val);
+    }
+}
