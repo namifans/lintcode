5bd3d37a1364d7b3972a343eb2d8b2b75aa85954
namifans <yanglin0617@gmail.com>
2017-09-22 00:08:47 -0700

    128 Hash function

---
+++ 128HashFunction/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param key: A String you should hash
+     * @param HASH_SIZE: An integer
+     * @return an integer
+     */
+    public int hashCode(char[] key,int HASH_SIZE) {
+        if (key == null || key.length == 0)
+            return 0;
+        long hash = 0;
+        for (int i = 0; i < key.length; i++) {
+            hash = hash * 33 + (int) key[i];
+            hash %= HASH_SIZE;
+        }
+        return (int) hash;
+    }
+};
