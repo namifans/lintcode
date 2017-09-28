1a2843d6a3c1b65c52b8acba8083326e05399b8b
namifans <yanglin0617@gmail.com>
2017-09-27 23:22:49 -0700

    073 Longest increasing subsequence

---
+++ 073LongestIncreasingSubsequence/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /*
+     * @param nums: An integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    public int longestIncreasingSubsequence(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+}
