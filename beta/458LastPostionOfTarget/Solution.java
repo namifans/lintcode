27170bb963c8c60f8f0b4852bf80302d25d60912
namifans <yanglin0617@gmail.com>
2017-10-01 20:16:14 -0700

    458 Last postion of target

---
+++ beta/458LastPostionOfTarget/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    /*
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int lastPosition(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (right > 0 && nums[right] == target)
+            return right;
+        if (left < nums.length && nums[left] == target) 
+            return left;
+        return -1;
+    }
+}
