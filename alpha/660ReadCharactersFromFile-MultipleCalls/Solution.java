622bb01e7c985f87c0631bc5c02485cb26f7362d
namifans <yanglin0617@gmail.com>
2017-10-19 01:29:48 -0700

    Fix 660

--- alpha/660ReadCharactersFromFile-MultipleCalls/Solution.java
+++ alpha/660ReadCharactersFromFile-MultipleCalls/Solution.java
@@ -3,33 +3,31 @@
 
 public class Solution extends Reader4 {
 
-    char[] buffer = new char[4];
-
+    Queue<Character> buffer = new LinkedList<>();
     /**
-     * @param buf destination buffer
-     * @param n maximum number of characters to read
-     * @return the number of characters read
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
      */
     public int read(char[] buf, int n) {
         int total = 0;
-        char[] tmp = new char[4];
         boolean eof = false;
-        for (int i = 0; i < 4; i++) {
-            if (buffer[i] != '\0') {
-                buf[total++] = buffer[i];
-                buffer[i] = '\0';
-            }
+        char[] tmp = new char[4];
+        while (total < n && !buffer.isEmpty()) {
+            Character c = buffer.poll();
+            buf[total++] = c;
         }
-        while (total < n && ! eof) {
+        while (total < n && !eof) {
             int read = read4(tmp);
-            if (read < 4)
+            if (read < 4) {
                 eof = true;
+            }
             int count = Math.min(read, n - total);
             for (int i = 0; i < count; i++) {
                 buf[total++] = tmp[i];
             }
             for (int i = 0; i < read - count; i++) {
-                buffer[i] = tmp[count + i];
+                buffer.add(tmp[count + i]);
             }
         }
         return total;
